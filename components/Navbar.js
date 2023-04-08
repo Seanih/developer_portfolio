fd6e0db74bc7557c39305013d6df36f5f1faa434
Sean Anih <sean.anih@gmail.com>
2023-04-08 18:29:07 -0400

    finished styling Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,24 @@
 function Navbar() {
-	return <nav>Navbar</nav>;
+	return (
+		<nav className='fixed w-full flex justify-around items-center py-6 bg-gradient-to-t from-black'>
+			<p className='text-3xl py-2 px-4 white-glow'>Sean Anih</p>
+
+			{/* nav links */}
+			<div className='flex gap-6 text-white/90 text-xl bg-black px-4'>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+					Skills
+				</div>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+					Projects
+				</div>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+					Contact
+				</div>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+					About
+				</div>
+			</div>
+		</nav>
+	);
 }
 export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Titillium_Web } from 'next/font/google';
+
+const titillium = Titillium_Web({
+	subsets: ['latin'],
+	weight: ['300', '400', '600', '700'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className={titillium.className}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
