b7e0cfc26328f198e72d44b6987c050a9d5f6b65
Sean Anih <sean.anih@gmail.com>
2023-04-08 18:56:49 -0400

    adjusted styling for nav links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,20 @@
 function Navbar() {
 	return (
-		<nav className='fixed w-full flex justify-around items-center py-6 bg-gradient-to-t from-black'>
-			<p className='text-3xl py-2 px-4 white-glow'>Sean Anih</p>
+		<nav className='fixed z-[100] w-full flex justify-between items-center py-6 bg-gradient-to-t from-black px-8 max-w-[1500px]'>
+			<p className='text-3xl white-glow'>Sean Anih</p>
 
 			{/* nav links */}
-			<div className='flex gap-6 text-white/90 text-xl bg-black px-4'>
-				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+			<div className='flex text-white/90 text-xl bg-black'>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8 px-4 border-r'>
 					Skills
 				</div>
-				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8 px-4 border-x'>
 					Projects
 				</div>
-				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8 px-4 border-x'>
 					Contact
 				</div>
-				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8'>
+				<div className='hover-link hover:cursor-pointer hover:underline hover: decoration-2 hover:underline-offset-8 px-4 border-l'>
 					About
 				</div>
 			</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	return (
-		<div>
+		<div className='flex flex-col items-center'>
 			<Head>
 				<title>Sean Anih || Developer Portfolio</title>
 				<meta name='description' content='My portfolio website' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-      <Navbar />
+			<Navbar />
 		</div>
 	);
 }
