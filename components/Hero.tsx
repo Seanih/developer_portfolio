d34bb98e6e12e3d1fe2e596c1562052f8bb5b7ae
Sean Anih <sean.anih@gmail.com>
2023-04-10 11:06:31 -0400

    changed typing animation timing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image';
 import demoPic from '../public/demo-pic.png';
 import { TypeAnimation } from 'react-type-animation';
 import Tilt from 'react-parallax-tilt';
+import { useState } from 'react';
 
 function Hero() {
+	const [animationCompleted, setAnimationCompleted] = useState(false);
+
 	return (
 		<section className='mt-16'>
 			<Tilt
@@ -20,13 +23,22 @@ function Hero() {
 					<div className='border-t rounded-xl grid sm:grid-cols-2 py-4 px-8'>
 						<div className='flex items-end md:justify-center mx-4 mb-4 sm:mb-0'>
 							<span className='font-bold text-xl'>Name: </span>
-							<div className='px-4 ml-4 border-b'>Sean Anih</div>
+							<TypeAnimation
+								sequence={['Sean Anih']}
+								speed={8}
+								cursor={false}
+								className='px-4 ml-4 border-b'
+							/>
 						</div>
 						<div className='flex items-end md:justify-center mx-4'>
 							<span className='font-bold text-xl'>Occupation: </span>
-							<div className='px-4 ml-4 border-b text-center'>
-								Full-Stack Developer
-							</div>
+							<TypeAnimation
+								sequence={['', 1000, 'FullStack Developer', 9250]}
+								speed={9}
+								cursor={false}
+								repeat={Infinity}
+								className='px-4 ml-4 border-b text-center'
+							/>
 						</div>
 					</div>
 					{/* Areas of focus */}
@@ -35,12 +47,14 @@ function Hero() {
 						<div className='px-4 text-center ml-4 border-b'>
 							<TypeAnimation
 								sequence={[
+									'',
+									3000,
 									'Web Development',
-									1500,
+									1750,
 									'Cloud Based Apps',
-									1500,
+									1750,
 									'Blockchain Apps',
-									1500,
+									1750,
 								]}
 								repeat={Infinity}
 							/>
