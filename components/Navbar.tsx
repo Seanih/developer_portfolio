bb3332139c92da4c80fafa455d0a44758d9abe6f
Sean Anih <sean.anih@gmail.com>
2023-04-28 20:54:32 -0400

    fixed z-index issue between Navbar & Hero components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,10 @@ interface NavbarProps {
 function Navbar({ showMobileNav, setShowMobileNav }: NavbarProps) {
 	return (
 		<>
-			<nav className='fixed z-[100] w-full bg-black/90 flex justify-between items-center py-6 px-12 max-w-[1500px]'>
+			<nav
+				className='fixed z-[100] w-full bg-black/90 flex justify-between items-center py-6 px-12 max-w-[1500px]'
+				style={{ transform: 'translateZ(200px)' }}
+			>
 				<Link
 					className='text-2xl sm:text-3xl white-glow hover:cursor-pointer'
 					to='hero'
